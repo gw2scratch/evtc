8da52c9ae736c8de9ac31ed9ba822cc3dd953c5b
Jiří Sejkora <sejsel@sejsel.cz>
2018-09-25 21:45:32 +0200

    Move BaseEncounter to its own file

---
+++ ScratchEVTCParser/Model/Encounters/BaseEncounter.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using System.Linq;
+using ScratchEVTCParser.Events;
+using ScratchEVTCParser.Model.Agents;
+using ScratchEVTCParser.Model.Encounters.Phases;
+
+namespace ScratchEVTCParser.Model.Encounters
+{
+	public class BaseEncounter : IEncounter
+	{
+		private readonly PhaseSplitter phaseSplitter;
+		private readonly IResultDeterminer resultDeterminer;
+		private readonly IEncounterNameProvider nameProvider;
+		private readonly Event[] events;
+
+		public BaseEncounter(IEnumerable<Agent> importantAgents, IEnumerable<Event> events, PhaseSplitter phaseSplitter,
+			IResultDeterminer resultDeterminer, IEncounterNameProvider nameProvider)
+		{
+			ImportantAgents = importantAgents.ToArray();
+			this.phaseSplitter = phaseSplitter;
+			this.resultDeterminer = resultDeterminer;
+			this.nameProvider = nameProvider;
+			this.events = events as Event[] ?? events.ToArray();
+		}
+
+		public IEnumerable<Agent> ImportantAgents { get; }
+
+		public EncounterResult GetResult()
+		{
+			return resultDeterminer.GetResult(events);
+		}
+
+		public IEnumerable<Phase> GetPhases()
+		{
+			return phaseSplitter.GetEventsByPhases(events);
+		}
+
+		public string GetName()
+		{
+			return nameProvider.GetEncounterName();
+		}
+	}
+}
--- ScratchEVTCParser/Model/Encounters/DefaultEncounter.cs
+++ ScratchEVTCParser/Model/Encounters/DefaultEncounter.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using System.Linq;
 using ScratchEVTCParser.Events;
 using ScratchEVTCParser.Model.Agents;
 using ScratchEVTCParser.Model.Encounters.Phases;
@@ -12,41 +11,6 @@ namespace ScratchEVTCParser.Model.Encounters
 			new PhaseSplitter(new StartTrigger("Default phase")), new AgentDeathResultDeterminer(boss),
 			new AgentNameEncounterNameProvider(boss))
 		{
-		}
-	}
-
-	public class BaseEncounter : IEncounter
-	{
-		private readonly PhaseSplitter phaseSplitter;
-		private readonly IResultDeterminer resultDeterminer;
-		private readonly IEncounterNameProvider nameProvider;
-		private readonly Event[] events;
-
-		public BaseEncounter(IEnumerable<Agent> importantAgents, IEnumerable<Event> events, PhaseSplitter phaseSplitter,
-			IResultDeterminer resultDeterminer, IEncounterNameProvider nameProvider)
-		{
-			ImportantAgents = importantAgents.ToArray();
-			this.phaseSplitter = phaseSplitter;
-			this.resultDeterminer = resultDeterminer;
-			this.nameProvider = nameProvider;
-			this.events = events as Event[] ?? events.ToArray();
-		}
-
-		public IEnumerable<Agent> ImportantAgents { get; }
-
-		public EncounterResult GetResult()
-		{
-			return resultDeterminer.GetResult(events);
-		}
-
-		public IEnumerable<Phase> GetPhases()
-		{
-			return phaseSplitter.GetEventsByPhases(events);
-		}
-
-		public string GetName()
-		{
-			return nameProvider.GetEncounterName();
 		}
 	}
 }
