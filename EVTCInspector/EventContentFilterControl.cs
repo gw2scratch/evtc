57f4d9f8e6f2e08d4cad08b2e526eeb60c7d8dd0
Jiří Sejkora <sejsel@sejsel.cz>
2020-05-08 16:19:29 +0200

    Inspector: Add a buff id filter for Buff Damage events

--- EVTCInspector/EventContentFilterControl.cs
+++ EVTCInspector/EventContentFilterControl.cs
@@ -9,12 +9,24 @@ namespace GW2Scratch.EVTCInspector
 	{
 		private uint BuffId { get; set; }
 		private bool BuffIdEnabled { get; set; }
+		private uint BuffDamageId { get; set; }
+		private bool BuffDamageIdEnabled { get; set; }
 
 		private readonly Control buffLayout;
+		private readonly Control buffDamageLayout;
 
 		public EventContentFilterControl()
 		{
-			buffLayout = BuildBuffLayout();
+			buffLayout = ConstructBuffLayout();
+			buffDamageLayout = ConstructBuffDamageLayout();
+			var layout = new DynamicLayout();
+			layout.BeginVertical();
+			{
+				layout.Add(buffLayout);
+				layout.Add(buffDamageLayout);
+			}
+			layout.EndVertical();
+			Content = layout;
 		}
 
 		public void UpdateContent(EventListControl eventList)
@@ -31,10 +43,9 @@ namespace GW2Scratch.EVTCInspector
 				commonAncestor = GetClosestType(commonAncestor, type.Type);
 			}
 
-			if (typeof(BuffEvent).IsAssignableFrom(commonAncestor))
-			{
-				Content = buffLayout;
-			}
+			buffLayout.Visible = typeof(BuffEvent).IsAssignableFrom(commonAncestor);
+			buffDamageLayout.Visible = typeof(BuffDamageEvent).IsAssignableFrom(commonAncestor);
+
 			/*
 			else if (typeof(AgentEvent).IsAssignableFrom(commonAncestor))
 			{
@@ -61,13 +72,9 @@ namespace GW2Scratch.EVTCInspector
 				filterPanel.Content = filterLayout;
 			}
 			*/
-			else
-			{
-				Content = null;
-			}
 		}
 
-		private Control BuildBuffLayout()
+		private Control ConstructBuffLayout()
 		{
 			var buffIdTextBox = new NumericMaskedTextBox<uint> {Value = 0};
 			buffIdTextBox.ValueBinding.Bind(() => BuffId, x => BuffId = x);
@@ -89,6 +96,28 @@ namespace GW2Scratch.EVTCInspector
 			return filterLayout;
 		}
 
+		private Control ConstructBuffDamageLayout()
+		{
+			var buffIdTextBox = new NumericMaskedTextBox<uint> {Value = 0};
+			buffIdTextBox.ValueBinding.Bind(() => BuffDamageId, x => BuffDamageId = x);
+
+			var buffIdCheckbox = new CheckBox {Checked = false};
+			buffIdCheckbox.CheckedBinding.Bind(() => BuffDamageIdEnabled, x => BuffDamageIdEnabled = x ?? false);
+
+			var filterLayout = new DynamicLayout();
+			filterLayout.BeginHorizontal();
+			{
+				filterLayout.BeginGroup("Buff damage");
+				{
+					filterLayout.AddRow("Buff ID", buffIdTextBox, buffIdCheckbox, null);
+				}
+				filterLayout.EndGroup();
+				filterLayout.AddRow(null);
+			}
+			filterLayout.EndHorizontal();
+			return filterLayout;
+		}
+
 		public bool FilterEvent(Event e)
 		{
 			if (e is BuffEvent buffEvent)
@@ -96,6 +125,14 @@ namespace GW2Scratch.EVTCInspector
 				if (BuffIdEnabled)
 				{
 					return buffEvent.Buff.Id == BuffId;
+				}
+			}
+
+			if (e is BuffDamageEvent buffDamage)
+			{
+				if (BuffDamageIdEnabled)
+				{
+					return buffDamage.Skill.Id == BuffDamageId;
 				}
 			}
 
