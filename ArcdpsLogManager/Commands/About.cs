c77da30b310f79647685400b1a7fe0ccbf8333a7
Jiří Sejkora <sejsel@sejsel.cz>
2020-05-11 01:17:02 +0200

    Manager: Images and their licenses

--- ArcdpsLogManager/Commands/About.cs
+++ ArcdpsLogManager/Commands/About.cs
@@ -15,11 +15,64 @@ namespace GW2Scratch.ArcdpsLogManager.Commands
 		{
 			base.OnExecuted(e);
 
+			string license = @"
+Summary
+
+- Software: MIT Licence
+- Profession and specialization images: Guild Wars 2 Wiki contributors, GNU FDL v1.2 or newer
+- Other images: ArenaNet, All Rights Reserved
+
+Profession and specialization images
+
+	Permission is granted to copy, distribute and/or modify this document
+	under the terms of the GNU Free Documentation License, Version 1.3
+	or any later version published by the Free Software Foundation;
+	with no Invariant Sections, no Front-Cover Texts, and no Back-Cover Texts.
+	A copy of the license is included in the section entitled ""GNU
+	Free Documentation License"".
+
+	For more details, see https://www.gnu.org/licenses/fdl-1.3.html.
+
+Non-profession images
+
+	©2010–2018 ArenaNet, LLC. All rights reserved. Guild Wars, Guild Wars 2,
+	Heart of Thorns, Guild Wars 2: Path of Fire, ArenaNet, NCSOFT, the Interlocking NC Logo,
+	and all associated logos and designs are trademarks or registered trademarks of NCSOFT Corporation.
+	All other trademarks are the property of their respective owners.
+
+	The Terms of Use are further detailed on the following URL:
+	https://www.guildwars2.com/en/legal/guild-wars-2-content-terms-of-use/
+
+The software
+
+	MIT License
+
+	Copyright (c) 2018
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the ""Software""), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.";
+
 			var about = new AboutDialog
 			{
 				Logo = Resources.GetProgramIcon(),
 				Website = new Uri("https://discord.gg/TnHpN34"),
-				WebsiteLabel = "Discord Server"
+				WebsiteLabel = "Discord Server",
+				License = license
 			};
 			about.ShowDialog(Application.Instance.MainForm);
 		}
--- ArcdpsLogManager/Resources.cs
+++ ArcdpsLogManager/Resources.cs
@@ -26,152 +26,152 @@ namespace GW2Scratch.ArcdpsLogManager
 
 		public static Image GetTinyIconWarrior()
 		{
-			return GetImage("Warrior_tango_icon_20px.png");
+			return GetImage("Tango/Warrior_tango_icon_20px.png");
 		}
 
 		public static Image GetTinyIconGuardian()
 		{
-			return GetImage("Guardian_tango_icon_20px.png");
+			return GetImage("Tango/Guardian_tango_icon_20px.png");
 		}
 
 		public static Image GetTinyIconRevenant()
 		{
-			return GetImage("Revenant_tango_icon_20px.png");
+			return GetImage("Tango/Revenant_tango_icon_20px.png");
 		}
 
 		public static Image GetTinyIconRanger()
 		{
-			return GetImage("Ranger_tango_icon_20px.png");
+			return GetImage("Tango/Ranger_tango_icon_20px.png");
 		}
 
 		public static Image GetTinyIconThief()
 		{
-			return GetImage("Thief_tango_icon_20px.png");
+			return GetImage("Tango/Thief_tango_icon_20px.png");
 		}
 
 		public static Image GetTinyIconEngineer()
 		{
-			return GetImage("Engineer_tango_icon_20px.png");
+			return GetImage("Tango/Engineer_tango_icon_20px.png");
 		}
 
 		public static Image GetTinyIconNecromancer()
 		{
-			return GetImage("Necromancer_tango_icon_20px.png");
+			return GetImage("Tango/Necromancer_tango_icon_20px.png");
 		}
 
 		public static Image GetTinyIconElementalist()
 		{
-			return GetImage("Elementalist_tango_icon_20px.png");
+			return GetImage("Tango/Elementalist_tango_icon_20px.png");
 		}
 
 		public static Image GetTinyIconMesmer()
 		{
-			return GetImage("Mesmer_tango_icon_20px.png");
+			return GetImage("Tango/Mesmer_tango_icon_20px.png");
 		}
 
 		public static Image GetTinyIconBerserker()
 		{
-			return GetImage("Berserker_tango_icon_20px.png");
+			return GetImage("Tango/Berserker_tango_icon_20px.png");
 		}
 
 		public static Image GetTinyIconSpellbreaker()
 		{
-			return GetImage("Spellbreaker_tango_icon_20px.png");
+			return GetImage("Tango/Spellbreaker_tango_icon_20px.png");
 		}
 
 		public static Image GetTinyIconDragonhunter()
 		{
-			return GetImage("Dragonhunter_tango_icon_20px.png");
+			return GetImage("Tango/Dragonhunter_tango_icon_20px.png");
 		}
 
 		public static Image GetTinyIconFirebrand()
 		{
-			return GetImage("Firebrand_tango_icon_20px.png");
+			return GetImage("Tango/Firebrand_tango_icon_20px.png");
 		}
 
 		public static Image GetTinyIconHerald()
 		{
-			return GetImage("Herald_tango_icon_20px.png");
+			return GetImage("Tango/Herald_tango_icon_20px.png");
 		}
 
 		public static Image GetTinyIconRenegade()
 		{
-			return GetImage("Renegade_tango_icon_20px.png");
+			return GetImage("Tango/Renegade_tango_icon_20px.png");
 		}
 
 		public static Image GetTinyIconDruid()
 		{
-			return GetImage("Druid_tango_icon_20px.png");
+			return GetImage("Tango/Druid_tango_icon_20px.png");
 		}
 
 		public static Image GetTinyIconSoulbeast()
 		{
-			return GetImage("Soulbeast_tango_icon_20px.png");
+			return GetImage("Tango/Soulbeast_tango_icon_20px.png");
 		}
 
 		public static Image GetTinyIconDaredevil()
 		{
-			return GetImage("Daredevil_tango_icon_20px.png");
+			return GetImage("Tango/Daredevil_tango_icon_20px.png");
 		}
 
 		public static Image GetTinyIconDeadeye()
 		{
-			return GetImage("Deadeye_tango_icon_20px.png");
+			return GetImage("Tango/Deadeye_tango_icon_20px.png");
 		}
 
 		public static Image GetTinyIconScrapper()
 		{
-			return GetImage("Scrapper_tango_icon_20px.png");
+			return GetImage("Tango/Scrapper_tango_icon_20px.png");
 		}
 
 		public static Image GetTinyIconHolosmith()
 		{
-			return GetImage("Holosmith_tango_icon_20px.png");
+			return GetImage("Tango/Holosmith_tango_icon_20px.png");
 		}
 
 		public static Image GetTinyIconReaper()
 		{
-			return GetImage("Reaper_tango_icon_20px.png");
+			return GetImage("Tango/Reaper_tango_icon_20px.png");
 		}
 
 		public static Image GetTinyIconScourge()
 		{
-			return GetImage("Scourge_tango_icon_20px.png");
+			return GetImage("Tango/Scourge_tango_icon_20px.png");
 		}
 
 		public static Image GetTinyIconTempest()
 		{
-			return GetImage("Tempest_tango_icon_20px.png");
+			return GetImage("Tango/Tempest_tango_icon_20px.png");
 		}
 
 		public static Image GetTinyIconWeaver()
 		{
-			return GetImage("Weaver_tango_icon_20px.png");
+			return GetImage("Tango/Weaver_tango_icon_20px.png");
 		}
 
 		public static Image GetTinyIconChronomancer()
 		{
-			return GetImage("Chronomancer_tango_icon_20px.png");
+			return GetImage("Tango/Chronomancer_tango_icon_20px.png");
 		}
 
 		public static Image GetTinyIconMirage()
 		{
-			return GetImage("Mirage_tango_icon_20px.png");
+			return GetImage("Tango/Mirage_tango_icon_20px.png");
 		}
 
 		public static Image GetTinyIconRaid()
 		{
-			return GetImage("raid_icon_32px.png");
+			return GetImage("ArenaNet/raid_icon_32px.png");
 		}
 
 		public static Image GetTinyIconFractals()
 		{
-			return GetImage("fractals_icon_32px.png");
+			return GetImage("ArenaNet/fractals_icon_32px.png");
 		}
 
 		public static Image GetTinyIconGuildRegistrar()
 		{
-			return GetImage("guild_registrar_icon_26px.png");
+			return GetImage("ArenaNet/guild_registrar_icon_26px.png");
 		}
 	}
 }
