92f10cf222ea97d81c135d178314a2f36e159502
Jiří Sejkora <sejsel@sejsel.cz>
2021-10-09 22:42:07 +0200

    Benchmarking: Fix reading logs on .NET 6
    https://github.com/dotnet/runtime/commit/68dec6ac17053b1d2c7bfead7e785c18ccdc8dd0

--- EVTCAnalytics.Benchmark/Program.cs
+++ EVTCAnalytics.Benchmark/Program.cs
@@ -33,7 +33,11 @@ namespace GW2Scratch.EVTCAnalytics.Benchmark
 					using var data = arch.Entries[0].Open();
 
 					var bytes = new byte[arch.Entries[0].Length];
-					data.Read(bytes, 0, bytes.Length);
+					int read;
+					int offset = 0;
+					while ((read = data.Read(bytes, offset, bytes.Length - offset)) > 0) {
+						offset += read;
+					}
 					logs.Add(bytes);
 				}
 			}
