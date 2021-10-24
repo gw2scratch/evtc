8224cf6f4e0d51003507c61760fc61a8e4f08d65
Jiří Sejkora <sejsel@sejsel.cz>
2021-10-24 04:03:59 +0200

    Update benchmarks

---
+++ EVTCAnalytics.Benchmark/Logs.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Reflection;
+
+namespace GW2Scratch.EVTCAnalytics.Benchmark;
+
+public static class Logs
+{
+	public static List<byte[]> LoadEmbeddedLogs()
+	{
+		var logs = new List<byte[]>();
+		foreach (var resourceName in Assembly.GetExecutingAssembly().GetManifestResourceNames())
+		{
+			if (resourceName.EndsWith(".zevtc"))
+			{
+				var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName);
+				using var arch = new ZipArchive(stream, ZipArchiveMode.Read);
+
+				if (arch.Entries.Count == 0)
+				{
+					throw new Exception("No EVTC file in ZIP archive.");
+				}
+
+				using var data = arch.Entries[0].Open();
+
+				var bytes = new byte[arch.Entries[0].Length];
+				int read;
+				int offset = 0;
+				while ((read = data.Read(bytes, offset, bytes.Length - offset)) > 0)
+				{
+					offset += read;
+				}
+
+				logs.Add(bytes);
+			}
+		}
+
+		if (logs.Count == 0)
+		{
+			throw new Exception("No logs found. Make sure that you have a logs directory next to the .csproj file when compiling, and only use .zevtc logs.");
+		}
+
+		return logs;
+	}
+}
--- EVTCAnalytics.Benchmark/Program.cs
+++ EVTCAnalytics.Benchmark/Program.cs
@@ -1,51 +1,23 @@
 ﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
 using BenchmarkDotNet.Running;
-using System;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
+using GW2Scratch.EVTCAnalytics.Model;
 using GW2Scratch.EVTCAnalytics.Processing;
 using System.Collections.Generic;
-using System.IO.Compression;
-using System.Reflection;
 
 namespace GW2Scratch.EVTCAnalytics.Benchmark
 {
-	public class EvtcAnalyticsBenchmark
+	[SimpleJob(RuntimeMoniker.NetCoreApp31)]
+	[SimpleJob(RuntimeMoniker.Net50)]
+	[SimpleJob(RuntimeMoniker.Net60)]
+	public class ProcessingBenchmark
 	{
-		private readonly List<byte[]> logs = new List<byte[]>();
+		private List<byte[]> logs;
 		
 		[GlobalSetup]
 		public void LoadLogs()
 		{
-			foreach (var resourceName in Assembly.GetExecutingAssembly().GetManifestResourceNames())
-			{
-				if (resourceName.EndsWith(".zevtc"))
-				{
-					var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName);
-					using var arch = new ZipArchive(stream, ZipArchiveMode.Read);
-
-					if (arch.Entries.Count == 0)
-					{
-						throw new Exception("No EVTC file in ZIP archive.");
-					}
-
-					using var data = arch.Entries[0].Open();
-
-					var bytes = new byte[arch.Entries[0].Length];
-					int read;
-					int offset = 0;
-					while ((read = data.Read(bytes, offset, bytes.Length - offset)) > 0) {
-						offset += read;
-					}
-					logs.Add(bytes);
-				}
-			}
-
-			if (logs.Count == 0)
-			{
-				throw new Exception("No logs found. Make sure that you have a logs directory next to the .csproj file when compiling, and only use .zevtc logs.");
-			}
+			logs = Logs.LoadEmbeddedLogs();
 		}
 
 		[GlobalCleanup]
@@ -55,17 +27,18 @@ namespace GW2Scratch.EVTCAnalytics.Benchmark
 		}
 
 		[Benchmark]
-		public void ParseAll()
+		public void DirectProcess()
 		{
 			var parser = new EVTCParser();
+			var processor = new LogProcessor();
 			foreach (var log in logs)
 			{
-				var parsed = parser.ParseLog(log);
+				var processed = processor.ProcessLog(log, parser);
 			}
 		}
 		
 		[Benchmark]
-		public void ParseAndProcessAll()
+		public void ParseAndProcess()
 		{
 			var parser = new EVTCParser();
 			var processor = new LogProcessor();
@@ -75,17 +48,41 @@ namespace GW2Scratch.EVTCAnalytics.Benchmark
 				var processed = processor.ProcessLog(parsed);
 			}
 		}
+	}
 
-		[Benchmark]
-		public void ParseAndProcessAndAnalyze()
+	[SimpleJob(RuntimeMoniker.NetCoreApp31)]
+	[SimpleJob(RuntimeMoniker.Net50)]
+	[SimpleJob(RuntimeMoniker.Net60)]
+	public class AnalyzerBenchmarks
+	{
+		private List<Log> logs;
+		
+		[GlobalSetup]
+		public void LoadLogs()
 		{
+			logs = new List<Log>();
+			var logBytes = Logs.LoadEmbeddedLogs();
 			var parser = new EVTCParser();
 			var processor = new LogProcessor();
+			foreach (var log in logBytes)
+			{
+				logs.Add(processor.ProcessLog(log, parser));
+			}
+		}
+
+		[GlobalCleanup]
+		public void RemoveLogs()
+		{
+			logs.Clear();
+		}
+		
+
+		[Benchmark]
+		public void Analyze()
+		{
 			foreach (var log in logs)
 			{
-				var parsed = parser.ParseLog(log);
-				var processed = processor.ProcessLog(parsed);
-				var analyzer = new LogAnalyzer(processed);
+				var analyzer = new LogAnalyzer(log);
 				analyzer.GetEncounter();
 				analyzer.GetMode();
 				analyzer.GetResult();
@@ -95,11 +92,13 @@ namespace GW2Scratch.EVTCAnalytics.Benchmark
 		}
 	}
 
+
 	internal class Program
 	{
 		public static void Main(string[] args)
 		{
-			BenchmarkRunner.Run<EvtcAnalyticsBenchmark>();
+			BenchmarkRunner.Run<ProcessingBenchmark>();
+			//BenchmarkRunner.Run<AnalyzerBenchmarks>();
 		}
 	}
 }
