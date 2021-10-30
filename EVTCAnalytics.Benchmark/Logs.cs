a8608fbdfd836472a73db6a6001a6ce805363757
Jiří Sejkora <sejsel@sejsel.cz>
2021-10-30 18:13:09 +0200

    Avoid file-scoped namespaces
    For now, we still build with .NET 5 in GH Actions.

--- EVTCAnalytics.Benchmark/Logs.cs
+++ EVTCAnalytics.Benchmark/Logs.cs
@@ -3,44 +3,45 @@ using System.Collections.Generic;
 using System.IO.Compression;
 using System.Reflection;
 
-namespace GW2Scratch.EVTCAnalytics.Benchmark;
-
-public static class Logs
+namespace GW2Scratch.EVTCAnalytics.Benchmark
 {
-	public static List<byte[]> LoadEmbeddedLogs()
+	public static class Logs
 	{
-		var logs = new List<byte[]>();
-		foreach (var resourceName in Assembly.GetExecutingAssembly().GetManifestResourceNames())
+		public static List<byte[]> LoadEmbeddedLogs()
 		{
-			if (resourceName.EndsWith(".zevtc"))
+			var logs = new List<byte[]>();
+			foreach (var resourceName in Assembly.GetExecutingAssembly().GetManifestResourceNames())
 			{
-				var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName);
-				using var arch = new ZipArchive(stream, ZipArchiveMode.Read);
-
-				if (arch.Entries.Count == 0)
+				if (resourceName.EndsWith(".zevtc"))
 				{
-					throw new Exception("No EVTC file in ZIP archive.");
+					var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName);
+					using var arch = new ZipArchive(stream, ZipArchiveMode.Read);
+
+					if (arch.Entries.Count == 0)
+					{
+						throw new Exception("No EVTC file in ZIP archive.");
+					}
+
+					using var data = arch.Entries[0].Open();
+
+					var bytes = new byte[arch.Entries[0].Length];
+					int read;
+					int offset = 0;
+					while ((read = data.Read(bytes, offset, bytes.Length - offset)) > 0)
+					{
+						offset += read;
+					}
+
+					logs.Add(bytes);
 				}
-
-				using var data = arch.Entries[0].Open();
-
-				var bytes = new byte[arch.Entries[0].Length];
-				int read;
-				int offset = 0;
-				while ((read = data.Read(bytes, offset, bytes.Length - offset)) > 0)
-				{
-					offset += read;
-				}
-
-				logs.Add(bytes);
 			}
-		}
 
-		if (logs.Count == 0)
-		{
-			throw new Exception("No logs found. Make sure that you have a logs directory next to the .csproj file when compiling, and only use .zevtc logs.");
-		}
+			if (logs.Count == 0)
+			{
+				throw new Exception("No logs found. Make sure that you have a logs directory next to the .csproj file when compiling, and only use .zevtc logs.");
+			}
 
-		return logs;
+			return logs;
+		}
 	}
 }
