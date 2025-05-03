dff91ba5f1a50fa750e81619c5e3b154ee7f7330
Linkaaaaa <christian.pagliarani@gmail.com>
2025-05-03 23:23:31 +0200

    [Analytics] [Inspector] Added new IdToGuid for Species and Skills

--- EVTCAnalytics/Model/Log.cs
+++ EVTCAnalytics/Model/Log.cs
@@ -43,6 +43,11 @@ namespace GW2Scratch.EVTCAnalytics.Model
 		public IReadOnlyList<Marker> Markers { get; }
 
 		/// <summary>
+		/// Provides a list of all <see cref="Specie"/>s that appear in the log.
+		/// </summary>
+		public IReadOnlyList<Specie> Species { get; }
+
+		/// <summary>
 		/// Provides a string with the version of arcdps used to record this log, prefixed with "EVTC".
 		/// </summary>
 		public string EvtcVersion { get; }
@@ -173,6 +178,7 @@ namespace GW2Scratch.EVTCAnalytics.Model
 			Skills = state.Skills;
 			Effects = state.EffectsById.Values.OrderBy(x => x.Id).ToList();
 			Markers = state.MarkersById.Values.OrderBy(x => x.Id).ToList();
+			Species = state.SpeciesById.Values.OrderBy(x => x.Id).ToList();
 			Errors = state.Errors;
 			InstanceStart = state.InstanceStart;
 			ArcdpsBuild = state.ArcdpsBuild;
@@ -182,7 +188,7 @@ namespace GW2Scratch.EVTCAnalytics.Model
 		/// Creates a new instance of a <see cref="Log"/> without requiring a <see cref="LogProcessorState"/>.
 		/// </summary>
 		internal Log(Agent mainTarget, LogType logType, IEnumerable<Event> events, IEnumerable<Agent> agents,
-			IEnumerable<Skill> skills, IEnumerable<Effect> effects, IEnumerable<Marker> markers, IEnumerable<LogError> errors, string arcdpsBuild,
+			IEnumerable<Skill> skills, IEnumerable<Effect> effects, IEnumerable<Marker> markers, IEnumerable<Specie> species, IEnumerable<LogError> errors, string arcdpsBuild,
 			IEncounterData encounterData, GameLanguage gameLanguage, string evtcVersion, LogTime startTime,
 			LogTime endTime, Player pointOfView, int? language, int? gameBuild, int? gameShardId, int? mapId,
 			InstanceStart instanceStart, int? fractalScale)
@@ -207,6 +213,7 @@ namespace GW2Scratch.EVTCAnalytics.Model
 			Skills = skills as Skill[] ?? skills.ToArray();
 			Effects = effects as Effect[] ?? effects.ToArray();
 			Markers = markers as Marker[] ?? markers.ToArray();
+			Species = species as Specie[] ?? species.ToArray();
 			Errors = errors as LogError[] ?? errors.ToArray();
 		}
 	}
